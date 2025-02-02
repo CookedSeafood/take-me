23102525e7d8a3f92d2280571640bd5f4657523d
CharcoalWhite <125453632+CharcoalWhite@users.noreply.github.com>
2025-02-02 16:52:48 +0800

    Fix crashes.
    - Fix crashes on trying to mount, dismount, or transfer players.
    - Fix all actions are available without both hands empty.

--- src/main/java/org/charcoalwhite/takeme/TakeMe.java
+++ src/main/java/org/charcoalwhite/takeme/TakeMe.java
@@ -3,6 +3,7 @@ package org.charcoalwhite.takeme;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.player.UseEntityCallback;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.ActionResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,25 +25,31 @@ public class TakeMe implements ModInitializer {
 		LOGGER.info("[TakeMe] *HeavyHeavyHeavy-*");
 
 		UseEntityCallback.EVENT.register((player, world, hand, entity, hitResult) -> {
-			if (!entity.isPlayer()) {
+			if (!(entity.isPlayer() && player.getMainHandStack().isEmpty() && player.getOffHandStack().isEmpty())) {
 				return ActionResult.PASS;
 			}
 
+			PlayerEntity player2 = (PlayerEntity) entity;
 			if (player.isSneaking()) {
 				if (!player.hasPassengers()) {
-					entity.startRiding(player, true);
+					player2.startRiding(player, true);
 				} else
-				if (player.hasPassenger(entity) && (int) player.getPitch() == 90) {
-					entity.stopRiding();
+				if (player.hasPassenger(player2) && (int) player.getPitch() == 90) {
+					player2.stopRiding();
 				} else {
 					Entity entity2 = player.getFirstPassenger();
-					if (!entity.hasPassengers()) {
-						entity2.startRiding(entity, true);
+					if (!entity2.isPlayer()) {
+						return ActionResult.PASS;
+					}
+
+					PlayerEntity player3 = (PlayerEntity) entity2;
+					if (!player2.hasPassengers()) {
+						player3.startRiding(player2, true);
 					}
 				}
 			} else
-			if (!entity.hasPassengers()) {
-				player.startRiding(entity, true);
+			if (!player2.hasPassengers()) {
+				player.startRiding(player2, true);
 			}
 			return ActionResult.PASS;
 		});
