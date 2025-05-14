d8b59298f603df6d84cdccefac59e45a38b4308b
CookedSeafood <125453632+CookedSeafood@users.noreply.github.com>
2025-05-14 11:08:08 +0800

    Update to 1.2.0.

--- src/main/java/net/cookedseafood/takeme/TakeMe.java
+++ src/main/java/net/cookedseafood/takeme/TakeMe.java
@@ -32,8 +32,8 @@ public class TakeMe implements ModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     public static final byte VERSION_MAJOR = 1;
-    public static final byte VERSION_MINOR = 1;
-    public static final byte VERSION_PATCH = 5;
+    public static final byte VERSION_MINOR = 2;
+    public static final byte VERSION_PATCH = 0;
 
     public static final boolean MAIN_HAND_FILTER_MODE = false;
     public static final boolean OFF_HAND_FILTER_MODE = false;
